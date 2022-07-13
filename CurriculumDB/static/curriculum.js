f74332f47717a26a5635ad2defc65cda9885cc9c
David Martin <DMAMartin@dundee.ac.uk>
2022-07-13 15:09:39 +0100

    starting point for JS components

---
+++ CurriculumDB/static/curriculum.js
@@ -0,0 +1,32 @@
+
+var getallprogrammes = function (callback){
+//TODO    
+    };
+
+var  getallmodules = function (callback){
+    //TODO
+    };
+    
+var getallactivities= function (callback){
+//TODO
+};
+
+var getallprogrammeILOs = function (callback){
+//TODO    
+    };
+
+var  getallmoduleILOs = function (callback){
+    //TODO
+    };
+    
+var getallactivityILOs= function (callback){
+//TODO
+};
+
+var addActivity = function (data, callback){};
+var addProgramme = function (data, callback){};
+var addModule = function (data, callback){};
+var addActivityILO = function (data, callback){};
+var addModuleILO = function (data, callback){};
+var addProgrammeILO = function (data, callback){};
+
